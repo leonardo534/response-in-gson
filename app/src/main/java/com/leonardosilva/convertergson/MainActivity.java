e7a90198922c4d5708292a6b7902355265e4532c
leonardo534 <leonardoredes534@gmail.com>
2020-03-24 10:55:41 -0300

    Adicionado metodo getPost() que recupera os dados

--- app/src/main/java/com/leonardosilva/convertergson/MainActivity.java
+++ app/src/main/java/com/leonardosilva/convertergson/MainActivity.java
@@ -1,6 +1,9 @@
 package com.leonardosilva.convertergson;
 
 import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,10 +12,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.leonardosilva.convertergson.api.DataService;
+import com.leonardosilva.convertergson.model.Posts;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private Button btnRecuperar;
     private TextView textoResultado;
     private Retrofit retrofit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +45,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getPosts() {
+        DataService dataService = retrofit.create(DataService.class);
+        Call<List<Posts>> call = dataService.getPosts();
+
+        call.enqueue(new Callback<List<Posts>>() {
+            @Override
+            public void onResponse(Call<List<Posts>> call, Response<List<Posts>> response) {
+                if (!response.isSuccessful()) {
+                    textoResultado.setText(response.message());
+                }
+                List<Posts> postsList = response.body();
+                textoResultado.setText("");
+                for (Posts posts : postsList) {
+                    String content = "";
+
+                    content += "\nUser id: " + posts.getUserId() + "\n";
+                    content += "Id post: " + posts.getId() + "\n";
+                    content += "Title: " + posts.getTitle() + "\n";
+                    content += "Post: " + posts.getBody() + "\n";
+
+                    textoResultado.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Posts>> call, Throwable t) {
+                textoResultado.setText(t.getMessage());
+            }
+        });
     }
 
 }
